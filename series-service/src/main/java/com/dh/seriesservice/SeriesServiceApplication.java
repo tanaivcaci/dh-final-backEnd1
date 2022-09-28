12e906a1c4f687f6a355aca0517f374df45dbe05
Tana Cingolani <tanaicc@gmail.com>
2022-09-27 22:12:03 -0300

    Initializr

---
+++ series-service/src/main/java/com/dh/seriesservice/SeriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dh.seriesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesServiceApplication.class, args);
+	}
+
+}
---
+++ server-config/src/main/java/com/dh/serverconfig/ServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.dh.serverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConfigApplication.class, args);
+	}
+
+}
