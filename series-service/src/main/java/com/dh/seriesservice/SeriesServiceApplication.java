adc01a92574d67868c74de9a4a2847f899ef5693
Tana Cingolani <tanaicc@gmail.com>
2022-10-01 02:05:27 -0300

    Eureka Discovery Client

--- series-service/src/main/java/com/dh/seriesservice/SeriesServiceApplication.java
+++ series-service/src/main/java/com/dh/seriesservice/SeriesServiceApplication.java
@@ -2,8 +2,10 @@ package com.dh.seriesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SeriesServiceApplication {
 
 	public static void main(String[] args) {
