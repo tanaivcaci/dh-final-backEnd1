b56c8fe8bd318cddc7a48b2847176177bd070fc9
Tana Cingolani <tanaicc@gmail.com>
2022-09-30 21:42:58 -0300

    config client y bootstrap dependencias

--- server-config/src/main/java/com/dh/serverconfig/ServerConfigApplication.java
+++ server-config/src/main/java/com/dh/serverconfig/ServerConfigApplication.java
@@ -2,8 +2,16 @@ package com.dh.serverconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/*
+@EnableConfigServer para habilitar el servidor de
+configuración y poder obtener
+las configuraciones desde el repo en github
+*/
 
 @SpringBootApplication
+@EnableConfigServer
 public class ServerConfigApplication {
 
 	public static void main(String[] args) {
